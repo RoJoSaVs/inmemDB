9d5b5000b8f199023524f5014c59c1dcab4e28b4
RoJoSaVs <43160386+RoJoSaVs@users.noreply.github.com>
2019-04-29 21:41:18 -0600

    Create BinaryTree.java

---
+++ src/Logic/BinaryTree/BinaryTree.java
@@ -0,0 +1,86 @@
+package Logic.BinaryTree;
+
+public class BinaryTree {
+    Node root;
+
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
+
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+
+        if (value < current.value) {
+            current.left = addRecursive(current.left, value);
+        } else if (value > current.value) {
+            current.right = addRecursive(current.right, value);
+        } else {
+            // value already exists
+            return current;
+        }
+
+        return current;
+    }
+
+    public boolean containsNode(int value) {
+        return containsNodeRecursive(root, value);
+    }
+
+    private boolean containsNodeRecursive(Node current, int value) {
+        if (current == null) {
+            System.out.println(false);
+            return false;
+        }
+        if (value == current.value) {
+            System.out.println(true);
+            return true;
+        }
+        return value < current.value
+                ? containsNodeRecursive(current.left, value)
+                : containsNodeRecursive(current.right, value);
+    }
+
+    public void delete(int value) {
+        root = deleteRecursive(root, value);
+    }
+
+    private Node deleteRecursive(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (value == current.value) {
+            // Node to delete found
+            // ... code to delete the node will go here
+            if (current.left == null && current.right == null) {
+                return null;
+            }
+            else if (current.right == null) {
+                return current.left;
+            }
+
+            else if (current.left == null) {
+                return current.right;
+            }
+            else{
+                int smallestValue = findSmallestValue(current.right);
+                current.value = smallestValue;
+                current.right = deleteRecursive(current.right, smallestValue);
+                return current;
+            }
+        }
+        if (value < current.value) {
+            current.left = deleteRecursive(current.left, value);
+            return current;
+        }
+        current.right = deleteRecursive(current.right, value);
+        return current;
+    }
+    private int findSmallestValue(Node root) {
+        return root.left == null ? root.value : findSmallestValue(root.left);
+    }
+
+
+}
