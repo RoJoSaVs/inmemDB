ecb1d60a4f43f8db5c505c707ee6f87798f90ad3
RoJoSaVs <43160386+RoJoSaVs@users.noreply.github.com>
2019-04-30 01:49:46 -0600

    Update Entrar.java

--- GUI/Entrar.java
+++ GUI/Entrar.java
@@ -2,11 +2,8 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
-import javax.swing.JComboBox;
-import javax.swing.JTable;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 public class Entrar extends JFrame{
 
@@ -60,4 +57,5 @@ public class Entrar extends JFrame{
         getContentPane().setBackground(Color.decode("#060734"));
         setVisible(true);
     }
+
 }
